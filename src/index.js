47f830938032dbec6e05e3e7bb4f91102b7c9f90
Code25 <chingisenkhbaatar@gmail.com>
2024-01-10 19:29:42 +0100

    added matrixrain as the background,and cursormove effect

---
+++ src/index.js
@@ -0,0 +1,69 @@
+const canvas = document.getElementById('Matrix');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const cyrillic = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ';
+const hangul = 'ㄱㄲㄳㄴㄵㄶㄷㄸㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅃㅄㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ';
+const devanagari = 'कखगघङचछजझञटठडढणतथदधनपफबभमयरलवशषसह';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+const unicodeString = '☯☮♻♲⚛⚕✝✞✟✠☥☦☧☨☩☸✡☪☫☬☭';
+
+const alphabet = katakana + cyrillic + hangul + devanagari + latin + nums + unicodeString;
+
+const fontSize = 16;
+const columns = canvas.width / fontSize;
+
+const rainDrops = [];
+
+
+for (let x = 0; x < columns; x++) {
+    rainDrops[x] = 1;
+}
+
+const draw = () => {
+    context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = '#0F0';
+    context.font = fontSize + 'px monospace';
+
+    // Draw raindrops
+    for (let i = 0; i < rainDrops.length; i++) {
+        const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
+
+        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+            rainDrops[i] = 0;
+        }
+        rainDrops[i]++;
+    }
+
+
+};
+
+const resizeCanvas = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+};
+
+setInterval(draw, 30);
+window.addEventListener('resize', resizeCanvas);
+
+// Mouse Effect
+const mouseEffect = (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    const dropIndex = Math.floor(mouseX / fontSize);
+    if (dropIndex >= 0 && dropIndex < rainDrops.length) {
+        rainDrops[dropIndex] = Math.floor(mouseY / fontSize);
+    }
+};
+
+
+window.addEventListener('mousemove', mouseEffect);
+
